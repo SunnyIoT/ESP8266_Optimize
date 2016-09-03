c5cfae073f05a401854d4cf7198c617e9ed78b27
Me <compilerexe@gmail.com>
2016-09-03 21:29:08 +0700

    Add feature send data lost connect to database.

--- ESP8266_Optimize/ESP8266_Optimize.ino
+++ ESP8266_Optimize/ESP8266_Optimize.ino
@@ -300,6 +300,34 @@ void loop() {
       Serial.println("connection lost, reconnect...");
 
       if (timer >= 5000) {
+
+        // Send data lost connect to database
+        if (!client.connect(host, httpPort)) {
+          Serial.println("connection failed");
+          return;
+        }
+
+        String url = "/laravel/public/device_lost_connect/";
+        url += ALIAS;
+
+        Serial.print("Requesting URL: ");
+        Serial.println(url);
+
+        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n" +
+                     "Connection: close\r\n\r\n");
+        unsigned long timeout = millis();
+        while (client.available() == 0) {
+          if (millis() - timeout > 5000) {
+            Serial.println(">>> Client Timeout !");
+            client.stop();
+            return;
+          }
+        }
+
+        Serial.println();
+        Serial.println("closing connection");
+
         microgear.connect(APPID);
         timer = 0;
         state_connect = 0;
